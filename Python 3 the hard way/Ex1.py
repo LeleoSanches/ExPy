a20447816685427e13a16252b3ddf7a3401aeeba
LeleoSanches <leosanches@uel.br>
2019-10-08 09:19:25 -0300

    teste

---
+++ Python 3 the hard way/Ex1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+print("Hello Again")
+
+
+# In[3]:
+
+
+print("I like typing this.")
+
+
+# In[4]:
+
+
+print('Yay! Printing')
+
+
+# In[5]:
+
+
+print("I'd much rather you 'not'.")
+
+
+# In[6]:
+
+
+print('I "said" do not touch this.')
+
+
+# In[ ]:
+
+
+
+
